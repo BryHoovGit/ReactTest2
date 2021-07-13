f22795f645995658e51628efbd46ba4dc3f4e6b2
Bryce Hoover <bryhoover11@gmail.com>
2021-07-13 13:14:17 -0500

    Added CommentDetail component

---
+++ src/components/CommentDetail.js
@@ -0,0 +1,21 @@
+import faker from 'faker';
+
+const CommentDetail = () => {
+    return (
+        <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {faker.name.firstName()}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">{faker.date.recent().toString()}</span>
+                    </div>
+                    <div className="text">{faker.hacker.phrase()}</div>
+                </div>
+        </div>
+)}
+
+export default CommentDetail
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './components/CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        {faker.name.firstName().toString()}
-                    </a>
-                    <div className="metadata">
-                        <span className="date">{faker.date.recent().toString()}</span>
-                    </div>
-                    <div className="text">{faker.hacker.phrase()}</div>
-                </div>
-            </div>
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div> 
     )};
 
